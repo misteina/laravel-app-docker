9d31a5d59c19f9574df1a6d4026af3fb28a8997b
Enyinna <enyinna.tech@gmail.com>
2021-02-27 19:38:28 +0100

    Refactor

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,11 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return [
+                'status' => 401,
+                'type' => 'error',
+                'message' => 'Unauthorized request'
+            ];
         }
     }
 }
