91b640640d554b25a2383ee90cd422199b125ebc
Enyinna <enyinna.tech@gmail.com>
2021-02-27 19:49:17 +0100

    Index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tests</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
+    <body>
+        <script src="tests.js"></script>
+    </body>
+</html>
